fc8f3919b43cd3ab1ad402e332bc1c14fae1f9a3
Laserzwei <lasernr2@gmail.com>
2019-01-11 22:07:01 +0100

    proper config version

--- mods/advShipyard/config/advshipyard.lua
+++ mods/advShipyard/config/advshipyard.lua
@@ -1,5 +1,16 @@
 config = {}
-config.VERSION = "1.10.0 - Avo-0.20.0"
+
+config.Author = "Laserzwei"
+config.ModName = "Advanced Shipyard"
+config.version = {
+    major=1, minor=10, patch = 0,
+    string = function()
+        return  Config.version.major .. '.' ..
+                Config.version.minor .. '.' ..
+                Config.version.patch
+    end
+}
+
 config.maxParallelShips = 6
 
 return config
