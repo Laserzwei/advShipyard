1ccd4ae75b4c3b56130b7d3ac91f584cdccf1cdd
Laserzwei <lasernr2@gmail.com>
2019-02-26 19:03:25 +0100

    1.10.2

--- mods/advShipyard/config/advshipyard.lua
+++ mods/advShipyard/config/advshipyard.lua
@@ -3,7 +3,7 @@ config = {}
 config.Author = "Laserzwei"
 config.ModName = "Advanced Shipyard"
 config.version = {
-    major=1, minor=10, patch = 1,
+    major=1, minor=10, patch = 2,
     string = function()
         return  Config.version.major .. '.' ..
                 Config.version.minor .. '.' ..
